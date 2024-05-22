27665a848ac1f5e16ec2e8a43dace0aebbdbdce7
johnnyfsa <joaofeirense@gmail.com>
2024-05-21 21:04:57 -0300

    adicionando manipulação de arquivos

--- src/br/com/alura/screenmatch/principal/MainWithSearch.java
+++ src/br/com/alura/screenmatch/principal/MainWithSearch.java
@@ -7,45 +7,58 @@ import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainWithSearch
 {
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner in = new Scanner(System.in);
-        System.out.println("Qual Título deseja buscar?");
-        var title = in.nextLine();
-        title.replace(" ","+");
-
-        String address = "http://www.omdbapi.com/?t=" + title +"&apikey=f6243ad3";
-
-        HttpClient client = HttpClient.newHttpClient();
-        HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create(address))
-                .build();
-        HttpResponse<String> response = client
-                .send(request, HttpResponse.BodyHandlers.ofString());
-        String json = response.body();
-        System.out.println(json);
-
-        Gson gson = new GsonBuilder()
+        String title = "";
+        List<Titulo> titulos = new ArrayList<>();
+        Gson gson;
+        gson = new GsonBuilder()
                 .setFieldNamingPolicy(FieldNamingPolicy.UPPER_CAMEL_CASE)
+                .setPrettyPrinting()
                 .create();
-        TituloOmdb myOmdbTitle = gson.fromJson(json, TituloOmdb.class);
-        System.out.println(myOmdbTitle);
-        try {
-            Titulo meuTitulo = new Titulo(myOmdbTitle);
-            System.out.println(meuTitulo);
-        }
-        catch (YearConversionException e)
-        {
-            System.out.println(e.getMessage());
-        }
+        while (true) {
+            System.out.println("Qual Título deseja buscar?");
+            title = in.nextLine();
+            if (title.equalsIgnoreCase("sair")) {
+                break;
+            }
+            title = title.replace(" ", "+");
 
+            String address = "http://www.omdbapi.com/?t=" + title + "&apikey=f6243ad3";
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(address))
+                    .build();
+            HttpResponse<String> response = client
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            String json = response.body();
+            System.out.println(json);
+
+
+            TituloOmdb myOmdbTitle = gson.fromJson(json, TituloOmdb.class);
+            try {
+                Titulo meuTitulo = new Titulo(myOmdbTitle);
+                titulos.add(meuTitulo);
+                System.out.println(meuTitulo);
+            } catch (YearConversionException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        FileWriter writer = new FileWriter("Titulos.json");
+        writer.write(gson.toJson(titulos));
+        writer.close();
     }
 }
